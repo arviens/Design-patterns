e2bd03a4d268c7cb9fb255e5a733283ff6c8f9e5
arviens <gezzerr@gmail.com>
2016-10-03 21:44:25 +0300

    Testing mode

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,9 +1,14 @@
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.Game;
 
-public class DesktopLauncher {
+public class Launcher {
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 400;
+		config.title = "NoodleJump";
 		new LwjglApplication(new Game(), config);
 	}
 }
---
+++ src/main/java/game/Game.java
@@ -0,0 +1,73 @@
+package game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Game extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture img;
+    private Music music;
+    private OrthographicCamera camera;
+    private BodyDef bodyDef;
+    private Rectangle bucket;
+    private World world;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("assets/images/batman.png");
+//        music = Gdx.audio.newMusic(Gdx.files.internal("assets/background.mp3"));
+//        music.setLooping(true);
+//        music.play();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2;
+        bucket.y = 20;
+        bucket.width = 47;
+        bucket.height = 83;
+        this.bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(100,300);
+        world = new World(new Vector2(0, -10), true);
+        Body body = world.createBody(bodyDef);
+        world.step(1/60f, 6, 2);
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        System.out.println(bodyDef.position.x);
+        batch.draw(img, bucket.x, bucket.y);
+        camera.update();
+        batch.end();
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) bucket.y += 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) bucket.y -= 200 * Gdx.graphics.getDeltaTime();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        music.dispose();
+        img.dispose();
+    }
+}
