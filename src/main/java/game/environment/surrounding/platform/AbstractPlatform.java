616a7b7007d64cf78be3fd879a6d3c1f0bb071e7
Arviens <gezzerr@gmail.com>
2016-10-29 13:48:26 +0300

    Abstract platform

--- src/main/java/game/environment/surrounding/platform/AbstractPlatform.java
+++ src/main/java/game/environment/surrounding/platform/AbstractPlatform.java
@@ -2,5 +2,8 @@ package game.environment.surrounding.platform;
 
 import game.environment.common.AbstractCollidable;
 
+/**
+ * Non-movable platform objects which might give damage to the player
+ */
 public abstract class AbstractPlatform extends AbstractCollidable {
 }
