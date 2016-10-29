d0bae082a980f8afd7fc6b1a446f76962691096c
kalafioras <et88568@gmail.com>
2016-10-29 14:48:52 +0100

    tokios i tokios

--- src/main/java/game/environment/surrounding/obstacle/AbstractObstacle.java
+++ src/main/java/game/environment/surrounding/obstacle/AbstractObstacle.java
@@ -5,5 +5,6 @@ import game.environment.common.AbstractCollidable;
 /**
  * Created by kalafioras on 2016-10-29.
  */
+// Not alive object, still or moving without an algorithm, harmful to player
 public class AbstractObstacle extends AbstractCollidable {
 }
