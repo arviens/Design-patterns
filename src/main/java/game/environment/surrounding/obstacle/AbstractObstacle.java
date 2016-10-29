91124dca1689c61f37a049755819991432a8842c
kalafioras <et88568@gmail.com>
2016-10-29 14:45:24 +0100

    tokios i tokios

---
+++ src/main/java/game/environment/surrounding/obstacle/AbstractObstacle.java
@@ -0,0 +1,9 @@
+package game.environment.surrounding.obstacle;
+
+import game.environment.common.AbstractCollidable;
+
+/**
+ * Created by kalafioras on 2016-10-29.
+ */
+public class AbstractObstacle extends AbstractCollidable {
+}
