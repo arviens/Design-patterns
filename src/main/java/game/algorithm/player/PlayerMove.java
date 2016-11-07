13424f178baa25b95ad0bf3bdb520e678c6ba8f4
Arviens <gezzerr@gmail.com>
2016-11-07 21:41:33 +0200

    sum fix

---
+++ src/main/java/game/algorithm/player/PlayerMove.java
@@ -0,0 +1,15 @@
+package game.algorithm.player;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import game.algorithm.Algorithm;
+import game.environment.abstractObject.common.AbstractCollidable;
+
+public class PlayerMove extends AbstractCollidable implements Algorithm {
+    public void move(AbstractCollidable object) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) object.setX(object.getX() - (int)(400 * Gdx.graphics.getDeltaTime()));
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))object.setX(object.getX() + (int)(400 * Gdx.graphics.getDeltaTime()));
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) object.setY(object.getY() + (int)(400 * Gdx.graphics.getDeltaTime()));
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) object.setY(object.getY() - (int)(400 * Gdx.graphics.getDeltaTime()));
+    }
+}
--- src/main/java/game/environment/object/player/Player.java
+++ src/main/java/game/environment/object/player/Player.java
@@ -1,15 +1,14 @@
 package game.environment.object.player;
 
+import game.algorithm.player.PlayerMove;
 import game.environment.abstractObject.player.AbstractPlayer;
 
-/**
- * Created by kalafioras on 2016-11-07.
- */
+
 public class Player extends AbstractPlayer {
     private final static String HEART_SPRITE = "src/main/resources/assets/images/gangsta.png";
 
     public Player() {
         setSprite(getSpriteByName(HEART_SPRITE));
-        setAlgorithm(null);
+        setAlgorithm(new PlayerMove());
     }
 }
